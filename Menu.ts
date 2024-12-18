7dc657d895497d3a9f290a6c137f5e0fbc7f9a7a
Vinicius Ribeiro <viniciusoliveira.270304@gmail.com>
2024-12-18 11:53:24 -0300

    :sparkles: criação do Menu

---
+++ Menu.ts
@@ -0,0 +1,70 @@
+import readlinesync = require("readline-sync");
+
+export function main() {
+
+    let opcao: number;
+    let nome : string;
+
+
+
+
+    console.log("")
+    do {
+
+        console.log("************************************************************")
+        console.log("\n                   Barbearia OldSchool Vini             \n")
+        console.log("************************************************************")
+        console.log("               1 - Cadastro de Cliente                      ")
+        console.log("               2 - Listar todos os Clientes                 ")
+        console.log("               3 - Consultar Cliente por ID                 ")
+        console.log("               4 - Atualizar Cliente                        ")
+        console.log("               5 - Deletar Cliente                          ")
+        console.log("               0 - Sair                                     ")
+        console.log("\n************************************************************\n")
+        opcao = readlinesync.questionInt("Digite a opcao desejada: ")
+
+        if (opcao === 0) {
+            console.log("Barbearia OldSchool Vini - Onde o Tratamento do Cliente vem em primeiro lugar! \n ")
+            about();
+            process.exit(0)
+        }
+
+        switch (opcao) {
+            case 1:
+                keyPress();
+                break;
+
+            case 2:
+                keyPress();
+                break;
+
+            case 3:
+                keyPress();
+                break;
+
+            case 4:
+                keyPress();
+                break;
+
+            case 5:
+                keyPress();
+                break;
+
+            default:
+                console.log("Opcao invalida!")
+        }
+
+    } while (true)
+
+}
+
+function keyPress(): void {
+    console.log("\nPressione enter para continuar...");
+    readlinesync.prompt();
+}
+
+
+function about() {
+    console.log("\nSistema criado pelo desenvolvedor Vinicius Oliveira.\n")
+}
+main()
